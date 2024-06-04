3038200ab8f8e9c60d8ce76211219e72a7a09d60
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-04 12:38:30 -0500

    controler and jsonbuilder

---
+++ app/views/foods/_food.json.jbuilder
@@ -0,0 +1,4 @@
+json.name = food.name
+json.ethnicity = food.ethnicity
+json.image_url = food.image_url
+json.time = food.time
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+food = Food.new(name: "Tsukune", ethnicity: "japanese", image_url:"https://www.seriouseats.com/thmb/iCcxmpH1j0g42B2RWnsmvcJB4zA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2014__10__20141017-tsukune-joshua-bousel-40e338184dd6464bae3ecad5353c5a41.jpg", time: 30)
+food.save
+food = Food.new(name: "Salted Pork", ethnicity: "Ship Rations", image_url:"https://thewoksoflife.com/wp-content/uploads/2017/01/salted-pork-5.jpg", time: 30)
+food.save
+food = Food.new(name: "Tzatziki", ethnicity: "greek", image_url:"https://joyfoodsunshine.com/wp-content/uploads/2016/06/tzatziki-sauce-recipe-1.jpg", time: 30)
+food.save
